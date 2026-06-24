5d627c23f5db8b1c3af3c1b3bdcf27023f0ee746
Farhan-Khan7 <fk5518744@gmail.com>
2026-06-25 03:49:57 +0530

    Today I have implement editor panel

--- script.js
+++ script.js
@@ -15,3 +15,183 @@ inputImg.addEventListener("change", function () {
     showImgsection.style.display = "flex";
 
 })
+
+// For apply filters
+
+
+const defaultFilters = {
+    brightness: 100,
+    contrast: 100,
+    saturation: 100,
+    grayscale: 0,
+    sepia: 0,
+    invert: 0,
+    blurimg: 0,
+    opacity: 100,
+    hueRotate: 0
+};
+
+const Filters = {
+    brightness: 100,
+    contrast: 100,
+    saturation: 100,
+    grayscale: 0,
+    sepia: 0,
+    invert: 0,
+    blurimg: 0,
+    opacity: 100,
+    hueRotate: 0
+};
+
+
+
+function applyFilters() {
+    previewImg.style.filter = `
+        brightness(${Filters.brightness}%)
+        contrast(${Filters.contrast}%)
+        saturate(${Filters.saturation}%)
+        grayscale(${defaultFilters.grayscale}%)
+        sepia(${Filters.sepia}%)
+        invert(${Filters.invert}%)
+        blur(${Filters.blurimg}px)
+        opacity(${Filters.opacity}%)
+        hue-rotate(${Filters.hueRotate}deg)
+    `;
+}
+
+
+// Brightness
+const brightnessControl = document.querySelector("#brightness input");
+const brightnessPercentage = document.querySelector("#brightness .value");
+
+brightnessControl.addEventListener("input", function () {
+    Filters.brightness = this.value;
+    brightnessPercentage.textContent = `${Filters.brightness}%`;
+    applyFilters();
+});
+
+// Contrast
+const contrastControl = document.querySelector("#contrast input");
+const contrastPercentage = document.querySelector("#contrast .value");
+
+contrastControl.addEventListener("input", function () {
+    Filters.contrast = this.value;
+    contrastPercentage.textContent = `${Filters.contrast}%`;
+    applyFilters();
+});
+
+// Saturation
+const saturationControl = document.querySelector("#saturation input");
+const saturationPercentage = document.querySelector("#saturation .value");
+
+saturationControl.addEventListener("input", function () {
+    Filters.saturation = this.value;
+    saturationPercentage.textContent = `${Filters.saturation}%`;
+    applyFilters();
+});
+
+// Grayscale
+const grayscaleControl = document.querySelector("#grayscale input");
+const grayscalePercentage = document.querySelector("#grayscale .value");
+
+grayscaleControl.addEventListener("input", function () {
+    Filters.grayscale = this.value;
+    grayscalePercentage.textContent = `${Filters.grayscale}%`;
+    applyFilters();
+});
+
+// Sepia
+const sepiaControl = document.querySelector("#sepia input");
+const sepiaPercentage = document.querySelector("#sepia .value");
+
+sepiaControl.addEventListener("input", function () {
+    Filters.sepia = this.value;
+    sepiaPercentage.textContent = `${Filters.sepia}%`;
+    applyFilters();
+});
+
+// Invert
+const invertControl = document.querySelector("#invert input");
+const invertPercentage = document.querySelector("#invert .value");
+
+invertControl.addEventListener("input", function () {
+    Filters.invert = this.value;
+    invertPercentage.textContent = `${Filters.invert}%`;
+    applyFilters();
+});
+
+// Blur
+const blurControl = document.querySelector("#blur input");
+const blurPercentage = document.querySelector("#blur .value");
+
+blurControl.addEventListener("input", function () {
+    Filters.blurimg = this.value;
+    blurPercentage.textContent = `${Filters.blurimg}px`;
+    applyFilters();
+});
+
+// Opacity
+const opacityControl = document.querySelector("#opacity input");
+const opacityPercentage = document.querySelector("#opacity .value");
+
+opacityControl.addEventListener("input", function () {
+    Filters.opacity = this.value;
+    opacityPercentage.textContent = `${Filters.opacity}%`;
+    applyFilters();
+});
+
+// Hue Rotate
+const hueRotateControl = document.querySelector("#hue-rotate input");
+const hueRotatePercentage = document.querySelector("#hue-rotate .value");
+
+hueRotateControl.addEventListener("input", function () {
+    Filters.hueRotate = this.value;
+    hueRotatePercentage.textContent = `${Filters.hueRotate}°`;
+    applyFilters();
+});
+
+
+
+// Reset All filters 
+
+
+
+
+function resetFilter() {
+Filters.brightness = defaultFilters.brightness;
+Filters.contrast = defaultFilters.contrast;
+Filters.saturation = defaultFilters.saturation;
+Filters.grayscale = defaultFilters.grayscale;
+Filters.sepia = defaultFilters.sepia;
+Filters.invert = defaultFilters.invert;
+Filters.blurimg = defaultFilters.blurimg;
+Filters.opacity = defaultFilters.opacity;
+Filters.hueRotate = defaultFilters.hueRotate;
+
+    brightnessControl.value = Filters.brightness
+    contrastControl.value = Filters.contrast
+    saturationControl.value = Filters.saturation
+    grayscaleControl.value = Filters.grayscale
+    sepiaControl.value = Filters.sepia
+    invertControl.value = Filters.invert
+    blurControl.value = Filters.blurimg
+    opacityControl.value = Filters.opacity
+    hueRotateControl.value = Filters.hueRotate
+
+    brightnessPercentage.textContent = `${Filters.brightness}%`;
+    contrastPercentage.textContent = `${Filters.contrast}%`;
+    saturationPercentage.textContent = `${Filters.saturation}%`;
+    grayscalePercentage.textContent = `${Filters.grayscale}%`;
+    sepiaPercentage.textContent = `${Filters.sepia}%`;
+    invertPercentage.textContent = `${Filters.invert}%`;
+    blurPercentage.textContent = `${Filters.blurimg}px`;
+    opacityPercentage.textContent = `${Filters.opacity}%`;
+    hueRotatePercentage.textContent = `${Filters.hueRotate}°`;
+}
+
+const resetFilters = document.querySelector("#reset-editing");
+
+resetFilters.addEventListener("click", function () {
+    resetFilter()
+    applyFilters()
+})
